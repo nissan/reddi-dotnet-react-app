656494ef8f5d27a6feebb3d9db1e623108ddb686
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-04 18:32:02 +1000

    adds storybook notes, but does not render properly

--- sampleApp/ClientApp/src/stories/App.stories.js
+++ sampleApp/ClientApp/src/stories/App.stories.js
@@ -4,8 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import { configure, addDecorator } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+addDecorator(withNotes);
+
 import App  from '../components/App';
 
+
 storiesOf('App', module)
-    .add('default', () => <App />)
-    .add('with different welcome message', () => <App welcomeMessage="Welcome with a different welcome message" />);;
+    .add('default', () => <App />, { notes: 'A default app page'})
+    .add('with different welcome message', () => <App welcomeMessage="Welcome with a different welcome message" />);
