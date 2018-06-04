2ca8a6e6520c8b0851d5ce00776f3a289effa4ce
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-04 19:16:33 +1000

    updates setup, minor edit on stories

--- sampleApp/ClientApp/src/stories/App.stories.js
+++ sampleApp/ClientApp/src/stories/App.stories.js
@@ -22,9 +22,23 @@ storiesOf('App', module)
             ReactDOM.render(<App />, div);
             ReactDOM.unmountComponentAtNode(div);
         });
+    })
+);
+return story;
+})
+    
+    .add('with custom welcome message', () => {
+    const welcomeMessage = "Welcome with a different welcome message";
+    const story = <App welcomeMessage={welcomeMessage} />;
+    specs(() => 
+        describe("App with custom welcome message", () => {
+        it("renders without crashing", () => {
+            const div = document.createElement("div");
+            ReactDOM.render(<App welcomeMessage={welcomeMessage} />, div);
+            ReactDOM.unmountComponentAtNode(div);
+        });
         it("should render the welcome message", () => {
-            const welcomeMessage = "Welcome to React!";
-            //'shallow' will not full-render 'react-emotion' components, so use 'mount' to test
+           //'shallow' will not full-render 'react-emotion' components, so use 'mount' to test
             const wrapper = mount(<App welcomeMessage={welcomeMessage} />); 
             const text = wrapper.find("h1").text();
             expect(text).toEqual(welcomeMessage);
@@ -32,6 +46,4 @@ storiesOf('App', module)
     })
 );
 return story;
-})
-    
-    .add('with different welcome message', () => <App welcomeMessage="Welcome with a different welcome message" />);;
+});;
