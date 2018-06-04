a73315d75121bb53da8bb7026419b9a257b79c44
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-04 19:09:03 +1000

    adds specs successfully

--- sampleApp/ClientApp/.storybook/config.js
+++ sampleApp/ClientApp/.storybook/config.js
@@ -1,5 +1,9 @@
 import { configure } from '@storybook/react';
 
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+
 const req = require.context('../src/stories', true, /\.stories\.js$/)
 
 function loadStories() {
--- sampleApp/ClientApp/src/stories/App.stories.js
+++ sampleApp/ClientApp/src/stories/App.stories.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import { specs, describe, it } from 'storybook-addon-specifications';
+import renderer from "react-test-renderer";
+import { mount } from "enzyme";
+import expect from 'expect';
+
 import App  from '../components/App';
 
 storiesOf('App', module)
-    .add('default', () => <App />)
+    .add('default', () => {
+    const story = <App />;
+    specs(() => 
+        describe("App", () => {
+        it("renders without crashing", () => {
+            const div = document.createElement("div");
+            ReactDOM.render(<App />, div);
+            ReactDOM.unmountComponentAtNode(div);
+        });
+        it("should render the welcome message", () => {
+            const welcomeMessage = "Welcome to React!";
+            //'shallow' will not full-render 'react-emotion' components, so use 'mount' to test
+            const wrapper = mount(<App welcomeMessage={welcomeMessage} />); 
+            const text = wrapper.find("h1").text();
+            expect(text).toEqual(welcomeMessage);
+          });
+    })
+);
+return story;
+})
+    
     .add('with different welcome message', () => <App welcomeMessage="Welcome with a different welcome message" />);;
