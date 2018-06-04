8cefd8afd2cc5cc37edeb0cf0d956e122d299744
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-04 17:26:41 +1000

    Adds first jest and enzyme tests

---
+++ sampleApp/ClientApp/src/tests/App.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it ('matches snapshot', () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("should render the welcome message", () => {
+  const welcomeMessage = "Welcome to React";
+  const wrapper = shallow(<App />); 
+  const text = wrapper.find("h1").text();
+  expect(text).toEqual(welcomeMessage);
+});
