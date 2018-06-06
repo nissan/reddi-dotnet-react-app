511e5d16019b3b2e86c42c79cd35fd5aacd61824
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-06-07 09:28:25 +1000

    adds jest-emotion for more consistent snapshot testing

--- sampleApp/ClientApp/src/setupTests.js
+++ sampleApp/ClientApp/src/setupTests.js
@@ -2,3 +2,8 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+import * as emotion from 'emotion'
+import { createSerializer } from 'jest-emotion'
+
+expect.addSnapshotSerializer(createSerializer(emotion))
